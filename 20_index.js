5d25a2d8ac38b8e2b5a18e2bee074adf2b41b57f
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-08-01 04:27:49 +0300

    Create 20_index.js

---
+++ 20_index.js
@@ -0,0 +1,16 @@
+let numOne = 100;
+let numTwo = 200;
+
+numOne > numTwo
+  ? console.log("1st > 2nd")
+  : numOne < numTwo
+  ? console.log("1st < 2nd")
+  : console.log("1st = 2nd");
+
+// if (numOne > numTwo) {
+//   console.log("1st > 2nd");
+// } else if (numOne < numTwo) {
+//   console.log("1st < 2nd");
+// } else {
+//   console.log("1st = 2nd");
+// }
