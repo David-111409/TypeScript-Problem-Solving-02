1b31f514f0cc7443630f936964903a9657fbc0cc
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-08-03 08:25:55 +0300

    Create 31_index.js

---
+++ 31_index.js
@@ -0,0 +1,4 @@
+console.log(
+    "%cHello World",
+    "color: white; height:100px;; background-color: blue; font-size: 2em; margin: 20px; display: flex; width: 300px; justify-content: center; align-items: center;"
+);
