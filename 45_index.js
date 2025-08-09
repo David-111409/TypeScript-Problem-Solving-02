f631b2020b53857a1abc48331cb4d48541650b75
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-08-09 19:42:30 +0300

    Create 45_index.js

---
+++ 45_index.js
@@ -0,0 +1,16 @@
+let text =
+        "Hello from Elzero Web School This is the type effect writer with js.",
+    i = 0,
+    myButton = document.getElementById("button");
+
+myButton.onclick = () => {
+    let writer = setInterval(() => {
+        document.getElementById("type").textContent += text[i];
+        i++;
+        if (i === text.length) {
+            clearInterval(writer);
+            myButton.removeEventListener("click");
+            i = 0;
+        }
+    }, 100);
+};
