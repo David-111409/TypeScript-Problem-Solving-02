edf4a4cdca8056b51921976000208450ca0c9f03
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-08-10 14:50:57 +0300

    Update 53_index.js

--- 53_index.js
+++ 53_index.js
@@ -4,6 +4,10 @@ const showTitle = document.getElementById("title-show");
 const showArea = document.getElementById("describe");
 
 // const updateText = (input, output) => (output.textContent = input.value);
+// document.addEventListener("contextmenu", (e) => {
+//     e.preventDefault();
+//     console.log("You clicked right click");
+// });
 
 function updateText(input, output) {
     output.textContent = input.value;
