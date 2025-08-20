7bf27bb43fa3f6f924b7802c9ecefb8e2d9f0dd9
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-08-20 11:18:03 +0300

    Create 75_index.js

---
+++ 75_index.js
@@ -0,0 +1,11 @@
+let myArr = [10, 10, 20, 20, 10, 30, 50, 20, 10];
+
+// myArr.length = 0;
+// myArr = [];
+// myArr.splice(myArr, myArr.length);
+while (myArr.length) {
+    myArr.pop();
+}
+console.log(myArr);
+// Output Needed
+// []
