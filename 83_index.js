1fffb02266364216cd9395992b68798d0c012d6e
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-08-23 20:23:00 +0300

    Create 83_index.js

---
+++ 83_index.js
@@ -0,0 +1,11 @@
+let strNumber = "10";
+
+console.log(strNumber * 1); // 10
+console.log(strNumber - 0); // 10 <= Same Solution
+console.log(+strNumber);
+console.log(Number(strNumber));
+console.log(parseFloat(strNumber));
+console.log(parseInt(strNumber));
+console.log(strNumber / 1);
+// Output Needed
+// 10
