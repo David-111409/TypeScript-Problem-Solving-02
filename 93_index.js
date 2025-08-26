417afe5cccdc8b2d9ea70e4f2602d836ce86316e
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-08-26 14:54:50 +0300

    Create 93_index.js

---
+++ 93_index.js
@@ -0,0 +1,16 @@
+let message = [];
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Meta") {
+        message.push("command");
+    }
+    if (e.key === "Alt") {
+        message.push("option");
+    }
+    if (e.key === "Shift") {
+        message.push("shift");
+    }
+    if (message.length === 3) {
+        console.log("You Pressed Ctrl + Alt + Shift");
+        message = [];
+    }
+});
